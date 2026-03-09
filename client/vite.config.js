70300b5347861eebe4a7c33cf136bd785bdb2aa5
Roman <romanvasenin@yandex.ru>
2026-03-09 18:52:02 +0300

    fix: small fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/socket.io': { target: 'http://localhost:3000', ws: true },
+      '/api': 'http://server:3000',
+      '/socket.io': { target: 'http://server:3000', ws: true },
     },
   },
 })
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -7,6 +7,7 @@ import GamePlayer from './GamePlayer.js';
 Room.belongsTo(User, { as: 'host', foreignKey: 'hostId' });
 Room.belongsToMany(User, { through: RoomPlayer, foreignKey: 'roomId', as: 'players' });
 User.belongsToMany(Room, { through: RoomPlayer, foreignKey: 'userId', as: 'rooms' });
+RoomPlayer.belongsTo(Room, { foreignKey: 'roomId' });
 
 Game.belongsTo(Room, { foreignKey: 'roomId' });
 Game.hasMany(GamePlayer, { foreignKey: 'gameId', as: 'gamePlayers' });
